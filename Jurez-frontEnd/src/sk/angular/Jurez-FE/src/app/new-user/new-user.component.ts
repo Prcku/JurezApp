ac867974e1d7e27bcfcd827a573ff1d6427edc36
branislav <branislavsocha159@gmail.com>
2023-02-19 17:55:28 +0100

    Pred updatom angular 14

--- Jurez-frontEnd/src/sk/angular/Jurez-FE/src/app/new-user/new-user.component.ts
+++ Jurez-frontEnd/src/sk/angular/Jurez-FE/src/app/new-user/new-user.component.ts
@@ -3,21 +3,27 @@ import {User} from "../user";
 import {UserService} from "../user.service";
 import {Router} from "@angular/router";
 import {NgForm} from "@angular/forms";
-
+// import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
 })
 export class NewUserComponent  {
+
+  // modalRef: BsModalRef | undefined;
   item = {} as User;
 
   constructor(private users: UserService,
               private router: Router,
+              //private modalService: BsModalService
                 ) {
 
   }
 
+  // openModal(template: TemplateRef<any>) {
+  //   this.modalRef = this.modalService.show(template);
+  // }
 
   submit(formElemnt: NgForm){
     if (formElemnt.invalid){
